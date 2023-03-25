e4fdea86131c5dcb4cc80025b0be9cdae9ef11f7
Carlosdvp <carlosdvp.tx@gmail.com>
2023-03-25 18:14:11 -0400

    updated the auth.service with a try-catch block to handle duplicate email errors.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from '../prisma/prisma.service';
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
+import { ForbiddenException } from "@nestjs/common";
 import { AuthDto } from "./dto";
 import * as argon from 'argon2';
 
@@ -10,18 +12,29 @@ export class AuthService {
 	async signup(dto: AuthDto) {
 		// 1. generate the passwrod hash
 		const hash = await argon.hash(dto.password);
-		// 2. save the user in the db
-		const user = await this.prisma.user.create({
-			data: {
-				email: dto.email,
-				hash,
-			},
-		});
+		
+		try {
+			// 2. save the user in the db
+			const user = await this.prisma.user.create({
+				data: {
+					email: dto.email,
+					hash,
+				},
+			});
 
-		delete user.hash;
-		// 3. return the saved user
-		return user;
+			delete user.hash;
+			// 3. return the saved user
+			return user;
+		} catch (error) {
+			if (error instanceof PrismaClientKnownRequestError) {
+				if (error.code === 'P2002') {
+					throw new ForbiddenException('A user already exists with that Email');
+				}
+			}
+			throw error;
+		}
 	}
+
 	
 	login() {
 		return { msg: 'I am logged in' }
